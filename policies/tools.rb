867facf8c658c98f431248340d2135908ca11fcb
Claudio Ortolina <claudio@jumpzero.com>
2012-01-13 14:44:40 +0000

    Split utils into utils/tools

--- policies/tools.rb
+++ policies/tools.rb
@@ -1,8 +1,9 @@
 require File.expand_path('./../../recipe_helper', __FILE__)
 
-policy :utils, :roles => :server do
-  requires :git
-  requires :build_essential
+policy :tools, :roles => :server do
+  requires :vim
+  requires :tree
+  requires :wget
 end
 
 deployment do
